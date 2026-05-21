5cd1c442aa5beab30dda86555ee0490bfffd3fe6
Sanchita <majumdarsanchita25@gmail.com>
2026-05-21 12:43:31 +0530

    Add files via upload

---
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './AppDownload.css'
+import { assets } from '../../assets/assets'
+
+const AppDownload = () => {
+    return (
+        <div className='app-download' id='app-download'>
+            <p>For Better Experience Download <br />TastySwift App</p>
+            <div className="app-download-platforms">
+                <a href='https://play.google.com/store/games?hl=en_IN&pli=1'><img src={assets.play_store} alt="" /></a>
+                <a href='https://apps.apple.com/in/iphone/apps'><img src={assets.app_store} alt="" /></a>
+            </div>
+        </div>
+    )
+}
+
+export default AppDownload
